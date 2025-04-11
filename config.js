c5296f832d9f65090a29965dbbb99490b07ed84e
crsampaio-saturno <crsampaio@uolinc.com>
2025-04-10 23:31:34 -0300

    improved settings screen

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  "GOOGLE_CLIENT_ID": "641475733495-ljdibcpq80hd5l9l4e63cu03653pbp33.apps.googleusercontent.com",
-  "GOOGLE_REDIRECT_URI": "http://localhost",
-  "GOOGLE_CLIENT_SECRET": "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",
-  "GH_TOKEN": "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"
-};
