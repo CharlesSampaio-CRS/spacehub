c92aedad83266879cdcb1bf9b3c52726a9c8be8a
crsampaio-saturno <crsampaio@uolinc.com>
2025-04-15 18:18:32 -0300

    removed configs

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  "GOOGLE_CLIENT_ID": "641475733495-ljdibcpq80hd5l9l4e63cu03653pbp33.apps.googleusercontent.com",
-  "GOOGLE_REDIRECT_URI": "http://localhost",
-  "GOOGLE_CLIENT_SECRET": "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",
-  "GH_TOKEN": "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"
-};
