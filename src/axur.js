9ce516bec1591c1bbd307e79b241bdc957046a1d
crsampaio-saturno <crsampaio@uolinc.com>
2025-05-15 11:12:28 -0300

    add

--- src/axur.js
+++ src/axur.js
@@ -1,80 +1,40 @@
-package com.axreng.backend.service;
+package com.axreng.backend.model;
 
-import com.axreng.backend.model.CrawlContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.Set;
+import java.util.concurrent.*;
 
-import java.util.List;
-import java.util.concurrent.ExecutorService;
+import static com.axreng.backend.model.enums.StatusSearchEnum.ACTIVE;
 
-import static com.axreng.backend.util.Constants.MAX_RESULTS;
+public class SearchResult {
+    private final String id;
+    private volatile String status;
+    private final Set<String> urls;
 
-public class CrawlerTask implements Runnable {
-
-    private static final Logger log = LoggerFactory.getLogger(CrawlerTask.class);
-
-    private final CrawlContext context;
-    private final ExecutorService executor;
-    private final CrawlerService crawlerService;
-
-    public CrawlerTask(CrawlContext context, ExecutorService executor, CrawlerService crawlerService) {
-        this.context = context;
-        this.executor = executor;
-        this.crawlerService = crawlerService;
+    public SearchResult(String id, String keyword) {
+        this.id = id;
+        this.urls = ConcurrentHashMap.newKeySet();
+        this.status = ACTIVE.name().toLowerCase();
     }
 
-    @Override
-    public void run() {
-        try {
-            while (!context.shouldStop()) {
-                String current = context.getToVisit().poll();
-                if (current == null) break;
-
-                if (!context.getVisited().add(current)) continue;
-
-                try {
-                    log.debug("Fetching HTML content for: {}", current);
-                    String html = crawlerService.fetchHtmlWithRetries(current).toLowerCase();
-
-                    if (context.shouldStop()) break;
-
-                    if (html.contains(context.getKeyword())) {
-                        synchronized (context.getResult()) {
-                            int currentSize = context.getResult().getUrls().size();
-
-                            if (currentSize < MAX_RESULTS) {
-                                context.getResult().addUrl(current);
-                                log.info("Keyword found at: {}", current);
-                                currentSize++; // Simula o incremento ao adicionar
-                            }
-
-                            if (currentSize >= MAX_RESULTS) {
-                                context.stop();
-                                break;
-                            }
-                        }
-                    }
-
-                    List<String> links = crawlerService.extractLinks(html, current);
-                    for (String link : links) {
-                        if (context.shouldStop()) break;
-
-                        if (link.startsWith(context.getBaseUrl()) &&
-                                context.getEnqueued().add(link)) {
-                            context.getToVisit().add(link);
-                        }
-                    }
-
-                    if (!context.shouldStop() && !context.getToVisit().isEmpty()) {
-                        executor.submit(new CrawlerTask(context, executor, crawlerService));
-                    }
-
-                } catch (Exception e) {
-                    log.warn("Error while processing URL: {} - {}", current, e.getMessage());
-                }
-            }
-        } finally {
-            context.getLatch().countDown();
-        }
+    public String getId() {
+        return id;
     }
+
+
+    public Set<String> getUrls() {
+        return urls;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void addUrl(String url) {
+        urls.add(url);
+    }
+
 }
