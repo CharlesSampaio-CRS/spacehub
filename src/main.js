18e0ede0a43acdac051e8e211eb9da2c27b5309f
CrsSaturno <ccharlesroberto@gmail.com>
2025-07-02 13:36:11 -0300

    removed log dev tools

--- src/main.js
+++ src/main.js
@@ -129,7 +129,7 @@ function createMainWindow() {
 
   mainWindow.loadFile(path.join(__dirname, 'pages/index/index.html'));
   mainWindow.maximize();
-  //mainWindow.setMenu(null);
+  mainWindow.setMenu(null);
 
   mainWindow.webContents.setWindowOpenHandler(({ url }) => {
     if (/^https?:\/\//.test(url) && !url.includes('linkedin.com')) {
@@ -179,7 +179,7 @@ function createLoginWindow() {
     }
   });
 
-  //loginWindow.setMenu(null);
+  loginWindow.setMenu(null);
   loginWindow.loadFile(path.join(__dirname, 'pages/login/login.html'));
   loginWindow.on('closed', () => { loginWindow = null; });
 }
